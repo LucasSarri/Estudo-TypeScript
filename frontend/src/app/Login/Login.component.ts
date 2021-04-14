c9947f88e320a644cbcbe44e27c184f541790ba7
LucasSarri <lucassarri@hotmail.com>
2021-04-14 00:43:37 -0300

    14/04

---
+++ frontend/src/app/Login/Login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-component-overview',
+    templateUrl: './Login.component.html',
+    styleUrls: ['./Login.component.css']
+})
+export class LoginComponent implements OnInit{
+
+    consturctor(){}
+
+    ngOnInit(): void{
+
+    }
+}
