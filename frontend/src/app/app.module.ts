4d2b84a6afb12cbca5b233287b6899cd7beacc84
LucasSarri <lucassarri@hotmail.com>
2021-04-16 00:08:00 -0300

    a sei la mano

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TesteComponent } from './teste/teste.component';
+import { LoginComponent } from './Login/Login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TesteComponent
+    TesteComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
